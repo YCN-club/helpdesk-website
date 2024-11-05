48fd6dd7c46c4af438e6edacaeb37278fda25c63
Abhigyan Trips <contact@abhigyantrips.dev>
2024-11-05 18:12:42 +0530

    fix(api): allow rewrite path to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async rewrites() {
+    return process.env.NODE_ENV === 'production'
+      ? [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:8000/:path*',
+          },
+        ]
+      : [];
+  },
 };
 
 export default nextConfig;
