29f4f88d06c9bd1b57cceb6ad0a0f049674e2691
Abhigyan Trips <contact@abhigyantrips.dev>
2024-11-16 19:25:35 +0530

    chore(config): hardcode IDs for showcase

---
+++ config/severity.ts
@@ -0,0 +1,3 @@
+export const severityConfig = {
+  '5809428e-efdc-450c-9048-8c7d0dced28f': 'Medium',
+};
---
+++ config/sla.ts
@@ -0,0 +1,8 @@
+export const slaConfig = [
+  {
+    id: 'ea8e3bec-a713-455e-9790-83f4beeaf887',
+    name: 'SLA 1',
+    time_limit: 24,
+    note: 'Test',
+  },
+];
