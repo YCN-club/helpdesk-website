7675d4f856eece9c4e969b0327e0f8333ef977b6
Abhigyan Trips <contact@abhigyantrips.dev>
2025-01-15 22:04:32 +0530

    fix(ui): shift dashboard to server-side render

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -2,7 +2,15 @@ import { Metadata } from 'next';
 
 import { getTickets } from '@/lib/actions/tickets';
 
-import DashboardPageClient from './page.client';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import { DataTable } from './data-table';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -11,5 +19,47 @@ export const metadata: Metadata = {
 export default async function DashboardPage() {
   const tickets = await getTickets(false);
 
-  return <DashboardPageClient tickets={tickets} />;
+  return (
+    <div className="mx-auto max-w-screen-lg py-6">
+      <div className="flex flex-col space-y-6">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                {tickets.filter((t) => t.ticket_status === 'OPEN').length}
+              </CardTitle>
+              <CardDescription>Open Tickets</CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                {tickets.filter((t) => t.ticket_status === 'CLOSED').length}
+              </CardTitle>
+              <CardDescription>Completed Tickets</CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                {tickets.filter((t) => t.assignee.name === 'Unassigned').length}
+              </CardTitle>
+              <CardDescription>Unassigned Tickets</CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Recent Tickets</CardTitle>
+            <CardDescription>
+              Search and manage your recent support tickets
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <DataTable data={tickets} />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
